8ea829287e36984c1285a8adbb65801dbe80e90f
sergioloff <82729586+sergioloff@users.noreply.github.com>
2021-09-09 10:51:33 +0100

    Project creation

---
+++ WHDxCacl.py
@@ -0,0 +1,211 @@
+from collections import namedtuple
+import torch
+import torch.autograd as tag
+import cupy as cp
+from cupy.cuda import function
+from pynvrtc.compiler import Program
+
+WHDx_forward_T_totThreads = 128
+code = '''
+// sum across dim col using a, x, sh_W[0..31]
+__forceinline__
+__device__ float ReduxDimCol(const float* sh_W, unsigned int a, unsigned int x)
+{
+    float sum = 0;
+    unsigned int aXORx = a ^ x;
+    for (int bix = 0; bix < 32; bix++, aXORx = aXORx >> 1)
+    {
+        sum += (aXORx & 0x1U) == 0 ? 0 : sh_W[bix];
+    }
+    return sum;
+}
+__global__ void WHDx_forward_cuda_T(float* WHDx_T, const unsigned int* uA_T, const unsigned int* uX_B_T, const float* W, int N, int M, int D)
+{
+    const int packDimBlocks = 4;
+    const int totThreads = WHDx_forward_T_totThreads;
+    if (blockDim.x != totThreads)
+        asm("trap;");
+    if (D % (4*32) != 0)
+        asm("trap;");
+    __shared__ float sh_W[totThreads * packDimBlocks];
+    int totDimCols = D / (packDimBlocks*32);
+    int m = blockDim.x * blockIdx.x + threadIdx.x;
+    int uA_ix = m;
+    int uX_ix = 0;
+    for (int dimCol = 0; dimCol < totDimCols; dimCol++)
+    {
+        __syncthreads(); 
+        if (threadIdx.x < 32 * packDimBlocks)
+            sh_W[threadIdx.x] = abs(W[dimCol*32*packDimBlocks+threadIdx.x]);
+        __syncthreads(); 
+        if (m < M)
+        {
+            unsigned int a1 = uA_T[uA_ix]; uA_ix+=M;
+            unsigned int a2 = uA_T[uA_ix]; uA_ix+=M;
+            unsigned int a3 = uA_T[uA_ix]; uA_ix+=M;
+            unsigned int a4 = uA_T[uA_ix]; uA_ix+=M;
+            for (int n = 0, outIx = m; n < N; n++, outIx+=M)
+            {
+                unsigned int x1 = uX_B_T[uX_ix++];
+                unsigned int x2 = uX_B_T[uX_ix++];
+                unsigned int x3 = uX_B_T[uX_ix++];
+                unsigned int x4 = uX_B_T[uX_ix++];
+                float sum = 0;
+                sum += ReduxDimCol(sh_W + 0 * 32, a1, x1);
+                sum += ReduxDimCol(sh_W + 1 * 32, a2, x2);
+                sum += ReduxDimCol(sh_W + 2 * 32, a3, x3);
+                sum += ReduxDimCol(sh_W + 3 * 32, a4, x4);
+                WHDx_T[outIx] += sum;
+            }
+        }
+    }
+}
+'''
+WHDx_backward_cuda_T_totThreads = 64
+code += '''
+__forceinline__
+__device__ float Sgn(const float x)
+{
+    return x > 0 ? 1.0 : (x < 0 ? -1.0 : 0.0);
+}
+
+__global__ void WHDx_backward_cuda_T(float* grad_W, const unsigned int* uA_T, const unsigned int* uX_T, const float* W, const float* grad_output, int N, int M, int D)
+{
+    if (D % 32 != 0) // 61
+        asm("trap;");
+    int laneId = threadIdx.x & 31;
+    int warpId = threadIdx.x / 32;
+    __shared__ float sh_grad_output[WHDx_backward_cuda_T_totThreads];
+    __shared__ unsigned int sh_Xt_dim[WHDx_backward_cuda_T_totThreads]; // each warp has its len 32 sub_array to cache 32 N's
+    float* sh_grad_output_warp = sh_grad_output + warpId * 32;
+    int d = blockDim.x * blockIdx.x + threadIdx.x;
+    int dBlock = d / 32;
+    unsigned int laneMask = 1U << laneId;
+    float sum = 0;
+    float sign_w = Sgn(W[d]);
+    unsigned int* sh_byWarp_Xt_dim = sh_Xt_dim + 32 * warpId;
+    const unsigned int* At_dim = uA_T + dBlock * M;
+    const unsigned int* Xt_dim = uX_T + dBlock * N;
+    for (int blockN = 0; blockN < N; blockN += 32)
+    {
+        sh_byWarp_Xt_dim[laneId] = Xt_dim[laneId + blockN];
+        for (int rowM = 0, go_ix = blockN + laneId; rowM < M; rowM++, go_ix += N)
+        {
+            unsigned int a = At_dim[rowM];
+
+            sh_grad_output[threadIdx.x] = grad_output[go_ix];
+            for (int elemInBlockN = 0; elemInBlockN < 32; elemInBlockN++)
+            {
+                float go = sh_grad_output_warp[elemInBlockN];
+                unsigned int x = sh_byWarp_Xt_dim[elemInBlockN];
+                unsigned int aMx = a ^ x;
+                sum += (aMx & laneMask) == 0 ? 0 : go * sign_w;
+            }
+        }
+    }
+    grad_W[d] = sum;
+}
+
+''' 
+code=code.replace("WHDx_forward_T_totThreads", str(WHDx_forward_T_totThreads))
+code=code.replace("WHDx_backward_cuda_T_totThreads", str(WHDx_backward_cuda_T_totThreads))
+cuda_func_names =  ["WHDx_forward_cuda_T", "WHDx_backward_cuda_T"]
+mod_cuda = cp.RawModule(code=code, options=('-std=c++11',), name_expressions=cuda_func_names)
+WHDx_forward_cuda_T = mod_cuda.get_function(cuda_func_names[0]) 
+WHDx_backward_cuda_T = mod_cuda.get_function(cuda_func_names[1]) 
+
+
+
+class WHDxFunction(tag.Function):
+    @staticmethod
+    # A:(M,D) X:(N,D) W:(D,)
+    def forward(ctx, A_bits_T, X_bits_T, X_bits_T_byDimBlock4, W): 
+        with torch.no_grad():
+            ctx.save_for_backward(A_bits_T, X_bits_T, W)
+            if (not X_bits_T.is_contiguous()):
+                raise Exception("X_bits_T must be contiguous")
+            if (not X_bits_T_byDimBlock4.is_contiguous()):
+                raise Exception("X_bits_T_byDimBlock4 must be contiguous")
+            if (not A_bits_T.is_contiguous()):
+                raise Exception("A_bits_T must be contiguous")
+            D = A_bits_T.shape[0]*32
+            if (D % (32*4) != 0):
+                raise Exception("D must be a multiple of 32*4")
+            M = A_bits_T.shape[1]
+            N = (X_bits_T.shape[0] * X_bits_T.shape[1]) // (D // 32)
+            totThreads = WHDx_forward_T_totThreads
+            totBlocks = M // totThreads 
+            if (M % totThreads != 0):
+                totBlocks += 1
+
+            ## slow, mem-inneficient version. uses original X and A, not the bit-packed version
+            # WHDx_T = ((A - X) * W).abs()
+
+            WHDx_T = torch.zeros((N,M), dtype=W.dtype, device=W.device, requires_grad=False) 
+            Stream = namedtuple('Stream', ['ptr'])
+            s = Stream(ptr=torch.cuda.current_stream().cuda_stream)
+            WHDx_forward_cuda_T(grid=(totBlocks,1,1), block=(totThreads,1,1), 
+                                args=[WHDx_T.data_ptr(), A_bits_T.data_ptr(), X_bits_T_byDimBlock4.data_ptr(), W.data_ptr(), N, M, D], 
+                                stream=s)
+            torch.cuda.synchronize()
+
+            WHDx = WHDx_T.T
+        return WHDx
+
+    @staticmethod
+    def backward(ctx, grad_output): 
+        with torch.no_grad():
+            grad_A = grad_X = grad_X_byDimBlock4 = grad_W = None
+            if ctx.needs_input_grad[0]:
+                    raise Exception("Can't differenciate wrt A")
+            if ctx.needs_input_grad[1]:
+                    raise Exception("Can't differenciate wrt X")
+            if ctx.needs_input_grad[2]:
+                    raise Exception("Can't differenciate wrt X_byDimBlock4")
+            if ctx.needs_input_grad[3]:
+                A_bits_T, X_bits_T, W = ctx.saved_tensors
+                if (not X_bits_T.is_contiguous()):
+                    raise Exception("X_bits_T must be contiguous")
+                if (not A_bits_T.is_contiguous()):
+                    raise Exception("A_bits_T must be contiguous")
+                if (not grad_output.is_contiguous()):
+                    grad_output = grad_output.contiguous()
+                D = A_bits_T.shape[0] * 32
+                M = A_bits_T.shape[1]
+                N = (X_bits_T.shape[0] * X_bits_T.shape[1]) // (D // 32)
+                if (N % 32 != 0):
+                    raise Exception("N % 32 != 0")
+                #totThreads = 1024 if D >= 45056 else 128
+                totThreads = 64
+                if (totThreads != WHDx_backward_cuda_T_totThreads):
+                    raise Exception("totThreads != WHDx_backward_cuda_T_totThreads")
+                if (totThreads % 32 != 0):
+                    raise Exception("totThreads % 32 != 0")
+                totBlocks = D // totThreads
+                if (D % totThreads != 0):
+                    raise Exception("D must be a multiple of totThreads")
+
+                ## slow, mem-inneficient version. uses original X and A, not the bit-packed version
+                #A_r = A.repeat_interleave(repeats=N, dim=0) # A_r:(M*N,D)
+                #X_ri = X.repeat(M, 1) # X_ri:(N*M,D)
+                #grad_W = (grad_output.view(M * N) * ((A_r - X_ri) * ((A_r - X_ri) * W).sign()) .T).sum(-1)
+
+                grad_W = torch.empty((D), dtype=W.dtype, device=W.device, requires_grad=False) 
+                Stream = namedtuple('Stream', ['ptr'])
+                s = Stream(ptr=torch.cuda.current_stream().cuda_stream)
+                WHDx_backward_cuda_T(grid=(totBlocks,1,1), block=(totThreads,1,1), 
+                                    args=[grad_W.data_ptr(), A_bits_T.data_ptr(), X_bits_T.data_ptr(), W.data_ptr(), grad_output.data_ptr(), N, M, D], 
+                                    stream=s)
+                torch.cuda.synchronize()
+
+            return grad_A, grad_X, grad_X_byDimBlock4, grad_W
+
+    @staticmethod
+    def GetDimBlock4(t_X_T_bits):
+        # TODO: there are better ways to do this...
+        t_tmp = torch.reshape(torch.empty_like(t_X_T_bits), (t_X_T_bits.shape[0] // 4, -1))
+        for dimIx in range(0, t_X_T_bits.shape[0], 4):
+            t_tmp[dimIx // 4] = t_X_T_bits[dimIx:dimIx + 4,:].T.flatten()
+        return torch.reshape(t_tmp, (-1,4))
+
+
---
+++ kNN_WHam.py
@@ -0,0 +1,197 @@
+import datetime
+import numpy as np
+import numpy.ma as ma
+from matplotlib import pyplot as plt
+import os
+import torch
+from torch import nn
+import cupy as cp
+from WHDxCacl import WHDxFunction
+from pytorch_lightning import seed_everything
+
+
+def LoadMNIST(folder):
+    # https://github.com/aiddun/binary-mnist/tree/master/original_28x28/binary_digits_binary_pixels/x_train.npy
+    # https://github.com/aiddun/binary-mnist/blob/master/original_28x28/binary_digits_all_pixels/y_train.npy
+    def Load(file_x, file_y):
+        x = np.load(file_x)
+        y = np.load(file_y).astype(dtype=np.float32)
+        x_padded = np.empty([x.shape[0],32,32], dtype=np.float32)
+        for ix in range(x.shape[0]):
+            pic = x[ix].reshape([28,28])
+            pic = np.pad(pic, pad_width=2, mode='constant', constant_values=0)
+            x_padded[ix] = pic.astype(dtype=np.float32)
+        return x_padded, y
+    x_train, y_train = Load(os.path.join(folder, 'x_train.npy'), os.path.join(folder, 'y_train.npy'))
+    x_test, y_test = Load(os.path.join(folder, 'x_test.npy'), os.path.join(folder, 'y_test.npy'))
+    return x_train, y_train, x_test, y_test 
+
+class kNNFit(nn.Module):
+    def __init__(self, D, eps, init_W=None):
+        super(kNNFit, self).__init__()
+        #W = np.random.uniform(size = [D]).astype(np.float32)
+        if (init_W is None):
+            init_W = np.ones([D], dtype=np.float32)
+        else:
+            init_W = init_W.astype(np.float32)
+        self.WHDxFunc = WHDxFunction.apply
+        self.t_W = nn.Parameter(torch.from_numpy(init_W))
+        self.alpha = nn.Parameter(torch.tensor(0, dtype=torch.float32))
+        self.beta = nn.Parameter(torch.tensor(1, dtype=torch.float32))
+        self.k = nn.Parameter(torch.tensor(0, dtype=torch.float32)) # horiz shift left/right [-1,1]
+        self.j = nn.Parameter(torch.tensor(0, dtype=torch.float32)) # curvature  [-1,1]
+        self.eps = eps
+
+    def forward(self, x):
+        t_X1_T_bits, t_X2_T_bits, t_X2_byDimBlock_T_bits, t_Y_knn = x
+        t_WHDx = self.WHDxFunc(t_X1_T_bits, t_X2_T_bits, t_X2_byDimBlock_T_bits, self.t_W) # ((x1 - x2) * W).abs()
+        t_norm_W = torch.linalg.norm(self.t_W, 1) + self.eps
+        t_FQx = self.F(1 - t_WHDx / t_norm_W) 
+        t_score = (t_FQx * torch.unsqueeze(t_Y_knn, 1)).mean(0) 
+        t_shiftedScore = self.beta * (t_score + self.alpha)
+        return t_shiftedScore
+
+    def F(self, x): 
+        # TODO: clamp x
+        inner = x * (1 - self.k) / (self.k * (1 - 2 * abs(x)) + 1)
+        outer = 0.5 + 0.5 * (inner * 2 - 1) * (1 + self.j) / (- self.j * (1 - 2 * abs(inner * 2 - 1)) + 1)
+        return outer 
+
+
+def GenerateEpochIxs(M):
+    return torch.randperm(M)
+
+def GenerateBatchIxs(epochIxs, batchLength, batchIx):
+    M = epochIxs.shape[0]
+    t_holdoutIxs = epochIxs[batchIx * batchLength:(batchIx + 1) * batchLength]
+    t_knnIxs = torch.cat([epochIxs[:batchIx * batchLength], epochIxs[(batchIx + 1) * batchLength:]])
+    return t_holdoutIxs, t_knnIxs 
+
+
+def GenerateBatchData(t_X_bits_T, t_Y, t_Y_01, t_holdoutIxs, t_knnIxs):
+    t_Y_knn = t_Y[t_knnIxs]
+    t_Y_holdout_01 = t_Y_01[t_holdoutIxs]
+    t_X_bits_T_knn = t_X_bits_T[:,t_knnIxs].contiguous()
+    t_X_bits_T_holdout = t_X_bits_T[:,t_holdoutIxs].contiguous()
+    t_X_bits_T_holdout_byDimBlock4 = WHDxFunction.GetDimBlock4(t_X_bits_T_holdout)
+    return t_X_bits_T_knn, t_Y_knn, t_X_bits_T_holdout, t_X_bits_T_holdout_byDimBlock4, t_Y_holdout_01
+
+
+def formatDateTime(dtn=None):
+    if dtn is None:
+        dtn = datetime.datetime.now()
+    return '{:04d}-{:02d}-{:02d} {:02d}:{:02d}:{:02d}'.format(dtn.year, dtn.month, dtn.day, dtn.hour,dtn.minute,dtn.second)
+
+
+if not torch.cuda.is_available():
+    raise Exception("cpu implementation not available")
+
+deviceIx = 0 
+torch_device = torch.device('cuda:' + str(deviceIx))
+cp_device = cp.cuda.Device(deviceIx)
+cp_device.use()
+
+seed_everything(seed = 0)
+eps = 1e-6
+loss_func = nn.BCEWithLogitsLoss(reduction="mean")
+opt_lr = 0.001
+
+X, Y_01, X_test, Y_01_test = LoadMNIST('c:\\lixo') #!!!!!
+
+batchLength = 64
+epochs = 3
+D = X.shape[1] * X.shape[2]
+totBatches = X.shape[0] // batchLength
+M = totBatches * batchLength
+X = X[:M]
+Y_01 = Y_01[:M]
+Y = np.where(Y_01 > 0.5, 1, -1).astype(np.float32)
+N = (X_test.shape[0] // 64) * 64
+X_test = X_test[:N]
+Y_01_test = Y_01_test[:N]
+Y_test = np.where(Y_01_test > 0.5, 1, -1).astype(np.float32)
+
+model = kNNFit(D, eps).to(torch_device)
+opt = torch.optim.Adam(model.parameters(), lr=opt_lr, eps=eps)
+
+print("tot attribs={}, tot train samples={}, tot test samples={}, tot batches={}, batchLength={}, tot epochs={}"
+    .format(D, M, N, totBatches, batchLength, epochs))
+
+if (M % batchLength != 0):
+    raise Exception("train set size must be divisible by batch length")
+
+# load all train&test data onto the gpu
+t_X_bits_T = torch.tensor(np.packbits(X.reshape([X.shape[0],-1]).astype(np.int32), axis=1).view(np.int32), device=torch_device).T.contiguous()
+t_Y = torch.tensor(Y, device=torch_device).contiguous()
+t_Y_01 = torch.tensor(np.where(Y >= 0, 1.0, 0.0).astype(np.float32), device=torch_device).contiguous()
+
+t_X_bits_T_test = torch.tensor(np.packbits(X_test.reshape([X_test.shape[0],-1]).astype(np.int32), axis=1).view(np.int32), device=torch_device).T.contiguous()
+t_X_bits_T_byDimBlock4_test = WHDxFunction.GetDimBlock4(t_X_bits_T_test)
+t_Y_test = torch.tensor(Y_test, device=torch_device).contiguous()
+t_Y_01_test = torch.tensor(Y_01_test, device=torch_device).contiguous()
+
+losses = []
+accs = []
+losses_test = []
+accs_test = []
+
+for epoch in range(epochs):
+    # a full epoch is the partitioning of the whole training datum indexes, shuffled, into "totBatches" batches
+    epochIxs = GenerateEpochIxs(M)
+    for batch in range(totBatches):
+        with torch.no_grad():
+            # each batch consists of the sampling of "batchLength" elements, without 
+            # replacement, from the indexes of the training set
+            # "t_holdoutIxs" are the set of indexes of the training data that will search for their nearest neighbours
+            # "t_knnIxs" are the set of indexes of the training data that will be used as knn neighbors
+            t_holdoutIxs, t_knnIxs = GenerateBatchIxs(epochIxs, batchLength, batch)
+            # get subsets of the training data on the gpu, based on the current batch's indexes
+            t_X_bits_T_knn, t_Y_knn, t_X_bits_T_holdout, t_X_bits_T_holdout_byDimBlock4, t_Y_holdout_01 = \
+                GenerateBatchData(t_X_bits_T, t_Y, t_Y_01, t_holdoutIxs, t_knnIxs)
+
+        t_predict_y = model([t_X_bits_T_knn, t_X_bits_T_holdout, t_X_bits_T_holdout_byDimBlock4, t_Y_knn])
+        
+        loss = loss_func(t_predict_y, t_Y_holdout_01)
+
+        loss.backward()
+        opt.step()
+        opt.zero_grad()
+
+        def CalcAcc(t_y_pred, t_y_true01):
+            t_y_pred01 = torch.where(t_y_pred > 0, 1, 0)
+            acc = (100.0 * (1.0 - abs(t_y_pred01 - t_y_true01).sum() / t_y_pred01.shape[0])).item()
+            return acc
+
+        with torch.no_grad():
+            # valid/test loss+acc. move to epoch end if performance is an issue
+            t_predict_y_test = model([t_X_bits_T, t_X_bits_T_test, t_X_bits_T_byDimBlock4_test, t_Y])
+            loss_test = loss_func(t_predict_y_test, t_Y_01_test).item()
+            acc_test = CalcAcc(t_predict_y_test, t_Y_01_test)
+      
+            acc = CalcAcc(t_predict_y, t_Y_holdout_01)
+            loss = loss.item()
+            losses.append(loss)
+            losses_test.append(loss_test)
+            accs.append(acc)
+            accs_test.append(acc_test)
+            print("{} epoch={:03d}, batch={:04d}, loss={:6.6f}, acc={:3.2f}%, test loss={:6.6f}, test acc={:3.2f}% "
+                  .format(formatDateTime(), epoch, batch, loss, acc, loss_test, acc_test))
+
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax2 = ax.twinx()
+lns1 = ax.plot(losses, color='black', label='train loss')
+lns2 = ax.plot(losses_test, color='grey', label='test loss')
+lns3 = ax2.plot(accs, color='red', label='train acc')
+lns4 = ax2.plot(accs_test, color='pink', label='test acc')
+lns = lns1+lns2+lns3+lns4
+labs = [l.get_label() for l in lns]
+ax.legend(lns, labs, loc='lower left')
+ax.grid()
+ax.set_xlabel("iteration")
+ax.set_ylabel("loss")
+ax2.set_ylabel("acc%")
+ax2.set_ylim(0, 100)
+plt.show()
+
